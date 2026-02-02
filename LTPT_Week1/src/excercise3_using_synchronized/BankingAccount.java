4d234b038efa1fa50503d263319aecff90bfd3d7
Le Thi Kim Ngan <23729131.ngan@student.iuh.edu.vn>
2026-02-02 10:01:34 +0700

    the banking app using keyword synchronized

---
+++ LTPT_Week1/src/excercise3_using_synchronized/BankingAccount.java
@@ -0,0 +1,66 @@
+package excercise3_using_synchronized;
+
+public class BankingAccount {
+
+
+    private String userName;
+    private double balance;
+
+    public BankingAccount(String userName, double balance) {
+        this.userName = userName;
+        this.balance = balance;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public synchronized void withdraw (double amount){
+        if (amount > balance){
+            try {
+                System.out.println("Balance < Amount");
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (amount < balance && amount > 0){
+            balance -= amount;
+            System.out.println(Thread.currentThread().getName() + " Success!" + balance);
+            notifyAll();
+        }
+
+    }
+
+    public synchronized void deposits (double amount){
+
+        if (balance + amount > 100_000){
+            try {
+                System.out.println("Balance + Amount > 100_000");
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (amount > 0){
+            balance += amount;
+            System.out.println(Thread.currentThread().getName()+ " Success! " + balance);
+            notifyAll();
+        }
+
+    }
+}
---
+++ LTPT_Week1/src/excercise3_using_synchronized/BankingApp.java
@@ -0,0 +1,21 @@
+package excercise3_using_synchronized;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BankingApp {
+    public static void main(String[] args) {
+        BankingAccount account = new BankingAccount("Kim Ngan", 10000);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        for (int i = 0; i < 10; i++){
+            executorService.submit(() -> account.withdraw(100));
+        }
+
+        executorService.shutdown();
+        while (!executorService.isTerminated()) {}
+
+        System.out.println("Finished!");
+    }
+}
