0ea99b113008c1096d4b2c8615e64927c9c3989b
Le Thi Kim Ngan <23729131.ngan@student.iuh.edu.vn>
2026-02-04 23:31:46 +0700

    update comment for code

--- LTPT_Week2/src/excercise1/MergeSortManualThread.java
+++ LTPT_Week2/src/excercise1/MergeSortManualThread.java
@@ -10,39 +10,40 @@ public class MergeSortManualThread {
         this.arr = arr;
     }
 
+
     private void sort () {
         if (arr.length <= THREAD_HOLD) {
-            Arrays.sort(arr);
+            Arrays.sort(arr); // neu mang nho thi su dung sort mang nhu binh thuong
             return;
         }
-        int mid = (arr.length) / 2;
-        int [] leftArr = new int [mid];
-        int [] rightArr = new int [arr.length - mid];
+        int mid = (arr.length) / 2; // chia mang ra lam 2
+        int [] leftArr = new int [mid]; // khoi tao mang luu tru phan ben trai
+        int [] rightArr = new int [arr.length - mid]; // khoi tao mang luu tru phan ben phai
         for (int i = 0; i < mid; i++) {
-            leftArr[i] = arr[i];
+            leftArr[i] = arr[i]; // copy mang ben trai
         }
 
         int j = 0;
         for (int i = mid; i < arr.length; i ++){
-            rightArr[j++] = arr[i];
+            rightArr[j++] = arr[i]; // copy mang ben phai
         }
 
-        MergeSortManualThread sortLeft = new MergeSortManualThread(leftArr);
-        MergeSortManualThread sortRight = new MergeSortManualThread(rightArr);
+        MergeSortManualThread sortLeft = new MergeSortManualThread(leftArr); // de quy sort mang trai
+        MergeSortManualThread sortRight = new MergeSortManualThread(rightArr); // de quy sort mang phai
 
-        Thread thread1 = new Thread(sortLeft::sort);
-        Thread thread2 = new Thread(sortRight::sort);
+        Thread thread1 = new Thread(sortLeft::sort); // goi thread thuc hien sort mang trai
+        Thread thread2 = new Thread(sortRight::sort); // goi thread thuc hien sort mang phai
 
         thread1.start();
         thread2.start();
 
         try {
-            thread1.join();
-            thread2.join();
+            thread1.join(); // goi pthuc join de thuc hien chan khong cho main thuc hien merge 2 mang lai
+            thread2.join(); // khi 2 thread chua sort xong
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        merge (leftArr, rightArr);
+        merge (leftArr, rightArr); // merge 2 mang lai voi nhau
     }
 
     private void merge (int [] leftArr, int [] rightArr) {
@@ -60,6 +61,9 @@ public class MergeSortManualThread {
         while (j < rightArr.length) result[k++] = rightArr[j++];
 
         System.arraycopy(result, 0, arr, 0, result.length);
+        // cu phap:
+        // System.arraycopy (nguon, vi tri nguon, dich, vi tri dich, so phan tu);
+
 
     }
 
